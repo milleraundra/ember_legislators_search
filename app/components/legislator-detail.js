db50cc8971f8cd2775a27601d13f14e0221ec48f
Aundra Miller and Michelle Brecunier <miller.aundra@gmail.com>
2016-04-05 12:03:41 -0700

    creating nested routes

--- app/components/legislator-detail.js
+++ app/components/legislator-detail.js
@@ -1,4 +1,18 @@
 import Ember from 'ember';
 
 export default Ember.Component.extend({
+  fullName: Ember.computed('legislator.first_name', 'legislator.last_name', function() {
+    return this.get('legislator.first_name') + " " + this.get('legislator.last_name');
+  }),
+  sponsoredBills: null,
+  actions: {
+    getBills(legislatorId) {
+      var url = 'http://congress.api.sunlightfoundation.com/bills/search?sponsor_id=' + legislatorId + '&apikey=20a6df84a0644ffd8281c431f6c623bd';
+      return Ember.$.getJSON(url).then(function(responseJSON) {
+        console.log(responseJSON.results);
+        this.set('sponsoredBills', responseJSON.results);
+        // return responseJSON.results;
+      })
+    }
+  }
 });
--- app/router.js
+++ app/router.js
@@ -7,6 +7,7 @@ const Router = Ember.Router.extend({
 
 Router.map(function() {
   this.route('results', {path: '/results/:state'});
+  this.route('legislator');
 });
 
 export default Router;
