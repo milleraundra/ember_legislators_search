39d0a31777b8a931390a634b78cd36d7343050f5
Aundra Miller and Michelle Brecunier <miller.aundra@gmail.com>
2016-04-05 14:49:08 -0700

    update api request to limit of 5 bills

--- app/components/legislator-container.js
+++ app/components/legislator-container.js
@@ -2,9 +2,17 @@ import Ember from 'ember';
 
 export default Ember.Component.extend({
   activeLegislator: null,
+  sponsoredBills: null,
   actions: {
     showLegislator(legislator) {
       this.set("activeLegislator", legislator);
+      var url = 'https://congress.api.sunlightfoundation.com/bills/search?sponsor_id=' + legislator.bioguide_id + '&per_page=5&apikey=20a6df84a0644ffd8281c431f6c623bd';
+      var that = this; //binding
+      return Ember.$.getJSON(url).then(function(responseJSON) {
+        return responseJSON.results;
+      }).then(function(results) {
+        return that.set('sponsoredBills', results);
+      });
     }
   }
 });
--- app/components/legislator-detail.js
+++ app/components/legislator-detail.js
@@ -3,20 +3,5 @@ import Ember from 'ember';
 export default Ember.Component.extend({
   fullName: Ember.computed('legislator.first_name', 'legislator.last_name', function() {
     return this.get('legislator.first_name') + " " + this.get('legislator.last_name');
-  }),
-  sponsoredBills: null,
-  actions: {
-    getBills(legislatorId) {
-      var url = 'https://congress.api.sunlightfoundation.com/bills/search?sponsor_id=' + legislatorId + '&apikey=20a6df84a0644ffd8281c431f6c623bd';
-      var that = this; //binding
-      return Ember.$.getJSON(url).then(function(responseJSON) {
-        return responseJSON.results;
-      }).then(function(results) {
-        return that.set('sponsoredBills', results);
-      });
-        // };
-        // this.set('sponsoredBills', apiFunction(url));
-        // console.log("hello", this.get('sponsoredBills'));
-    }
-  }
+  })
 });
